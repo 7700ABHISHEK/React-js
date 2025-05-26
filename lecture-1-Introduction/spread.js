487c68eb5b632726237bc8d60ccb7cc245cc2bde
Abhishek dubey <7700.Abhishekdubey02@gmai.com>
2025-05-26 15:59:23 +0530

    useRef

---
+++ lecture-1-Introduction/spread.js
@@ -0,0 +1,34 @@
+// with array :- 1
+
+// let arr = [1,2,3,4,5];
+
+// console.log(...arr);
+
+
+// With object :- 2
+
+// let obj = {
+//     name: "Abhi",
+//     class: "First Class",
+//     standard: "High Standard"
+// }
+
+// let newObj = {...obj};
+
+// newObj.name = "Suraj"
+// newObj.class = "Low Class"
+// newObj.standard = "Poor Standard"
+
+// console.log(newObj, obj);
+
+
+// With Function :- 3
+
+// function sum(a, b, c) {
+//     return a + b + c;
+// }
+
+// let numbers = [1, 2, 3];
+// let newSum = sum(...numbers);
+
+// console.log(newSum);
